6ba2601db94a8772612d21d69148a4dd628f2fb9
GreysonAgnello <agngre1130A@mischools.org>
2026-02-08 18:04:47 -0500

    Fixed Gyro output and prepared Intake Pt.1

--- src/main/java/frc/robot/subsystems/drive/GyroIONavX.java
+++ src/main/java/frc/robot/subsystems/drive/GyroIONavX.java
@@ -7,30 +7,43 @@
 
 package frc.robot.subsystems.drive;
 
+import static edu.wpi.first.units.Units.Degrees;
+import static edu.wpi.first.units.Units.DegreesPerSecond;
+import static edu.wpi.first.units.Units.RadiansPerSecond;
 import static frc.robot.subsystems.drive.DriveConstants.*;
 
-import com.studica.frc.AHRS;
-import com.studica.frc.AHRS.NavXComType;
+import com.studica.frc.Navx;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.util.Units;
+import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
 import java.util.Queue;
+import java.util.function.DoubleSupplier;
 
 /** IO implementation for NavX. */
 public class GyroIONavX implements GyroIO {
-  private final AHRS navX = new AHRS(NavXComType.kMXP_SPI, (byte) odometryFrequency);
+  private final Navx navX = new Navx(0);
+  private AngularVelocity[] Velocity;
   private final Queue<Double> yawPositionQueue;
   private final Queue<Double> yawTimestampQueue;
 
   public GyroIONavX() {
     yawTimestampQueue = SparkOdometryThread.getInstance().makeTimestampQueue();
-    yawPositionQueue = SparkOdometryThread.getInstance().registerSignal(navX::getAngle);
+    //yawPositionQueue = SparkOdometryThread.getInstance().registerSignal(navX::getAngle);
+    yawPositionQueue = SparkOdometryThread.getInstance().registerSignal(() -> navX.getYaw().in(Degrees));
   }
 
   @Override
   public void updateInputs(GyroIOInputs inputs) {
-    inputs.connected = navX.isConnected();
-    inputs.yawPosition = Rotation2d.fromDegrees(-navX.getAngle());
-    inputs.yawVelocityRadPerSec = Units.degreesToRadians(-navX.getRawGyroZ());
+    inputs.connected = true;
+    inputs.yawPosition = Rotation2d.fromDegrees(navX.getYaw().in(Degrees));
+   // inputs.yawVelocityRadPerSec = Units.degreesToRadians(-navX.getAngularVel()[3].in(DegreesPerSecond));
+    Velocity = navX.getAngularVel();
+    inputs.yawVelocityRadPerSec = Velocity[2].in(DegreesPerSecond);
+
+    SmartDashboard.putNumber("GyroAngle", navX.getYaw().in(Degrees));
+    SmartDashboard.putNumber("GyroZ", Velocity[2].in(DegreesPerSecond));
 
     inputs.odometryYawTimestamps =
         yawTimestampQueue.stream().mapToDouble((Double value) -> value).toArray();
